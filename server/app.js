6eea2c45cc9ca415b3529b7a8c450562de48ee2e
galsim <thespacemaks@gmil.com>
2019-11-17 14:56:43 +0300

    lesson 6

--- server/app.js
+++ server/app.js
@@ -2,8 +2,25 @@ const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
+const m = (anme, text, id) => ({
+  name, text, id
+})
+
 io.on('connection', socket => {
-  console.log('IO Connected')
+  
+  socket.on('userJoined', (data, callback) =>{ 
+    if (!data.name || !data.room) {
+      return callback('Данные некоректны')
+    } 
+
+    socket.join(data.room)
+
+    cb({ userId: socket.id })
+    socket.emit('newMessage', m('admin', `Добро пожаловать ${data.name}`))
+    socket.broadcast
+      .to(data.room)
+      .emit('admin', `Пользователь ${data.name} вошел в комнату`)
+  })
 
   socket.on('createMessage', data => {
     setTimeout(() => {
